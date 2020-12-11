beb042b0fdb8a21797aec74b9f951150e025467c
rachwongrw <rwrachelwong@gmail.com>
2020-12-10 23:19:17 -0800

    fix styling of projects, edit styling

--- portfolio/src/app/service/project.service.ts
+++ portfolio/src/app/service/project.service.ts
@@ -6,7 +6,7 @@ import { Project } from '../projects/project.model';
 })
 export class ProjectService {
   projects: Project[] = [
-    new Project(1, 'Hoopstir', "React, Node, AWS, MongoDB", "Hoopstir is a Sports Software application designed to support and automate the administrative tasks required for basketball. This project is predominantly built in React, Node and Mongo DB.For this pwork in a full stack capacity, with extra focus on the front end. Due to client confidentiality, the repository is private.", "../../assets/images/hoopstir-1.png"),
+    new Project(1, 'Hoopstir', "React, Node, AWS, MongoDB", "Hoopstir is a Sports Software application designed to support and automate the administrative tasks required for basketball. This project is predominantly built in React, Node and Mongo DB. For this project, I worked in a full stack capacity, with extra focus on the front end. Due to client confidentiality, the repository is private.", "../../assets/images/hoopstir-1.png"),
     new Project(2, 'Client PWA', "React, Node", 'Building a PWA application for a client from scratch. Primarily using React and Node. This applications was build using React Hooks. Due to client confidentiality, this repository is private.', "../../assets/images/hoopstir-1.png"),
     new Project(3, 'Student Works', "API, Ruby", `Predominantly worked with the Sign Request API `, "../../assets/images/hoopstir-1.png"),
     new Project(4, 'Group Project In Angular', "Angular, Node, JS", 'This was originally a school group project where we created an ecommerce website. This was converted from Vanilla JS to Angular.', "../../assets/images/hoopstir-1.png"),
