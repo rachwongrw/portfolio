83446256a53adb74e3cec353de540bd4f3ad27fb
rachwongrw <rwrachelwong@gmail.com>
2020-12-14 23:19:10 -0800

    add resume and links to page, styling edits, add potential portrait img options

--- portfolio/src/app/service/project.service.ts
+++ portfolio/src/app/service/project.service.ts
@@ -7,11 +7,11 @@ import { Project } from '../projects/project.model';
 export class ProjectService {
   projects: Project[] = [
     new Project(1, 'Hoopstir', "React, Node, AWS, MongoDB", "Hoopstir is a Sports Software application designed to support and automate the administrative tasks required for basketball. This project is predominantly built in React, Node and Mongo DB. For this project, I worked in a full stack capacity, with extra focus on the front end. Due to client confidentiality, the repository is private.", "../../assets/images/hoopstir-1.png"),
-    new Project(2, 'Client PWA', "React, Node", 'Building a PWA application for a client from scratch. Primarily using React and Node. This applications was build using React Hooks. Due to client confidentiality, this repository is private.', "../../assets/images/hoopstir-1.png"),
-    new Project(3, 'Student Works', "API, Ruby", `Predominantly worked with the Sign Request API `, "../../assets/images/hoopstir-1.png"),
-    new Project(4, 'Group Project In Angular', "Angular, Node, JS", 'This was originally a school group project where we created an ecommerce website. This was converted from Vanilla JS to Angular.', "../../assets/images/hoopstir-1.png"),
-    new Project(5, '.NET eCommerce Website', ".NET, SQL", 'Built an ecommerce site using Razor pages, authentication and authorization', "../../assets/images/hoopstir-1.png"),
-    new Project(6, 'Project 6', "React, Ruby", 'Hoopstir is a Sports Software application designed to support and automate the administrative tasks required for basketball. This project is predominantly built in React, Node and Mongo DB. I predominantly worked in a full stack capacity, with extra focus on the front end', "../../assets/images/hoopstir-1.png")
+    new Project(2, 'Client PWA', "React, Node", 'Building a PWA application for a client from scratch. Primarily using React and Node. This applications was built using React Hooks. Due to client confidentiality, this repository is private.', "../../assets/images/placeholder-project.jpg"),
+    new Project(3, 'Student Works', "API, Ruby", `Predominantly worked with the Sign Request API `, "../../assets/images/placeholder-project.jpg"),
+    new Project(4, 'Group Project In Angular', "Angular, Node, JS", 'This was originally a school group project where we created an snowboarding ecommerce website. This was converted from Vanilla JS to Angular.', "../../assets/images/suki.png"),
+    new Project(5, '.NET eCommerce Website', ".NET, SQL", 'Built an ecommerce site using Razor pages with authentication and authorization', "../../assets/images/hoopstir-1.png"),
+    new Project(6, 'Project 6', "React, Ruby", 'Hoopstir is a Sports Software application designed to support and automate the administrative tasks required for basketball. This project is predominantly built in React, Node and Mongo DB. I predominantly worked in a full stack capacity, with extra focus on the front end', "../../assets/images/placeholder-project.jpg")
   ]
   constructor() { } 
 
